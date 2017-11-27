5a0622732a0c22e7a4fcb4887d7852f560224b12
zao1214 <hexa1214@gmail.com>
2017-11-27 17:20:33 +0900

    Add project

---
+++ Source/TankBrawl/TankBrawlGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankBrawlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKBRAWL_API ATankBrawlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
